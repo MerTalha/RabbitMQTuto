5eab9f49619e85c0749c9012011436a6278723b7
Mert Şimşek <merttalhasimsek@gmail.com>
2024-04-29 09:48:10 +0300

    Add project files.

---
+++ Udemy-RabbitMQ.publisher/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://qgvajewc:zIYAn4-pkcFlUsba0UjT4WRKhdHwle3U@goose.rmq2.cloudamqp.com/qgvajewc");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+
+
