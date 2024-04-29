f25a797b11e83bd4f12b9824aff1addbcdb55c9e
Mert Şimşek <merttalhasimsek@gmail.com>
2024-04-29 10:32:51 +0300

    Work Queue Update

--- Udemy-RabbitMQ.publisher/Program.cs
+++ Udemy-RabbitMQ.publisher/Program.cs
@@ -1,13 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RabbitMQ.Client;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://qgvajewc:zIYAn4-pkcFlUsba0UjT4WRKhdHwle3U@goose.rmq2.cloudamqp.com/qgvajewc");
 
 using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+    Console.WriteLine($"Gönderildi: {message}");
+});
+
+Console.ReadLine();
+
 
 
 
--- UdemyRabbiMQ.subscriber/Program.cs
+++ UdemyRabbiMQ.subscriber/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://qgvajewc:zIYAn4-pkcFlUsba0UjT4WRKhdHwle3U@goose.rmq2.cloudamqp.com/qgvajewc");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+channel.BasicQos(0,1,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", false, consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine(message);
+
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+
+
+Console.ReadLine();
