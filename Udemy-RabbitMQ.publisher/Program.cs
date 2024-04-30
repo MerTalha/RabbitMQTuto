ed9314ea5b6baa7ac90cd798aa7a679cb68ee232
Mert Şimşek <merttalhasimsek@gmail.com>
2024-04-30 10:13:12 +0300

    Fanout Exchange Update

--- Udemy-RabbitMQ.publisher/Program.cs
+++ Udemy-RabbitMQ.publisher/Program.cs
@@ -9,15 +9,17 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
-channel.QueueDeclare("hello-queue", true, false, false);
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
 
 Enumerable.Range(1, 50).ToList().ForEach(x =>
 {
-    string message = $"Message {x}";
+    string message = $"Log {x}";
 
     var messageBody = Encoding.UTF8.GetBytes(message);
 
-    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    channel.BasicPublish("logs-fanout", "", null, messageBody);
 
     Console.WriteLine($"Gönderildi: {message}");
 });
--- UdemyRabbiMQ.subscriber/Program.cs
+++ UdemyRabbiMQ.subscriber/Program.cs
@@ -12,11 +12,15 @@ var channel = connection.CreateModel();
 
 //channel.QueueDeclare("hello-queue", true, false, false);
 
+var randomQueueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
 channel.BasicQos(0,1,false);
 
 var consumer = new EventingBasicConsumer(channel);
 
-channel.BasicConsume("hello-queue", false, consumer);
+channel.BasicConsume(randomQueueName, false, consumer);
 
 consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
 {
