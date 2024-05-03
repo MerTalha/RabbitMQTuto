0334a5d18f9e45ca385dc63292586405cb566360
Mert Şimşek <merttalhasimsek@gmail.com>
2024-05-03 09:51:22 +0300

    RabbitMQ Client Service Update

--- UdemyRabbitMQWeb-Watermark/Program.cs
+++ UdemyRabbitMQWeb-Watermark/Program.cs
@@ -1,10 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System.Configuration;
 using UdemyRabbitMQWeb_Watermark.Models;
+using UdemyRabbitMQWeb_Watermark.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(builder.Configuration.GetConnectionString("RabbitWQ"))});
+builder.Services.AddSingleton<RabbitMQClientService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseInMemoryDatabase(databaseName: "productDb");
