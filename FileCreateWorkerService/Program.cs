5f8f751e3dbf41b0cb05c5aeab9bb9be1aab556e
Mert Şimşek <merttalhasimsek@gmail.com>
2024-05-08 11:16:46 +0300

    Database First Update

--- FileCreateWorkerService/Program.cs
+++ FileCreateWorkerService/Program.cs
@@ -1,5 +1,7 @@
 using FileCreateWorkerService;
+using FileCreateWorkerService.Models;
 using FileCreateWorkerService.Services;
+using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
 
 
@@ -8,8 +10,12 @@ using RabbitMQ.Client;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services )=>
     {
-        IConfiguration configuration = context.Configuration; 
+        IConfiguration configuration = context.Configuration;
 
+        services.AddDbContext<AdventureWorks2019Context>(opt =>
+        {
+            opt.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+        });
 
         services.AddSingleton<RabbitMQClientService>();
         services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(configuration.GetConnectionString("RabbitMQ")), DispatchConsumersAsync = true });
