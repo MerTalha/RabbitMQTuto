76ca568e20a0cee05c6e8fb89389e6d1da34e656
Mert Şimşek <merttalhasimsek@gmail.com>
2024-05-08 10:21:49 +0300

    WorkerService Update

--- FileCreateWorkerService/Program.cs
+++ FileCreateWorkerService/Program.cs
@@ -1,9 +1,20 @@
 using FileCreateWorkerService;
+using FileCreateWorkerService.Services;
+using RabbitMQ.Client;
+
+
+
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services )=>
     {
+        IConfiguration configuration = context.Configuration; 
+
+
+        services.AddSingleton<RabbitMQClientService>();
+        services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(configuration.GetConnectionString("RabbitMQ")), DispatchConsumersAsync = true });
         services.AddHostedService<Worker>();
+
     })
     .Build();
 
