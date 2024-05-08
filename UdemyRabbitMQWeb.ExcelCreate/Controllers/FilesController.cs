384e56999a1857fceb7592e8df403a385d8811b7
Mert Şimşek <merttalhasimsek@gmail.com>
2024-05-08 13:33:02 +0300

    File Controller Update

---
+++ UdemyRabbitMQWeb.ExcelCreate/Controllers/FilesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UdemyRabbitMQWeb.ExcelCreate.Models;
+
+namespace UdemyRabbitMQWeb.ExcelCreate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public FilesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload (IFormFile file, int fileId)
+        {
+            if (file is not { Length: > 0 }) return BadRequest();
+            
+            var userFile = await _appDbContext.UserFiles.FirstAsync(x => x.Id == fileId);
+
+            var filePath = userFile.FileName + Path.GetExtension(file.FileName);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", filePath);
+
+            using FileStream stream = new(path, FileMode.Create);
+
+            await file.CopyToAsync(stream);
+
+            userFile.CreatedDate = DateTime.Now;
+            userFile.FilePath = filePath;
+            userFile.FileStatus = FileStatus.Completed;
+            
+            await _appDbContext.SaveChangesAsync();
+            //SignalR Notification oluşturulacak
+
+            return Ok();
+
+        }
+    }
+}
